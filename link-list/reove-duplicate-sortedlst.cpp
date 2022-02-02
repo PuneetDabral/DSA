35a148a2c796c379156f4a8dd820382216e05df2
Puneet Dabral <puneetdabral2000@gmail.com>
2022-02-02 10:50:34 +0530

    remove duplicate in sorted list

---
+++ link-list/reove-duplicate-sortedlst.cpp
@@ -0,0 +1,41 @@
+/************************************************************
+
+    Following is the linked list node structure.
+    
+    class Node 
+    {
+        public:
+        int data;
+        Node* next;
+
+        Node(int data) 
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+    
+************************************************************/
+
+Node * uniqueSortedList(Node * head) {
+    // Write your code here.
+    if(head==NULL){
+        //empty list 
+        return NULL;
+    }
+    Node *curr = head;
+    
+    while(curr!=NULL){
+        if((curr->next!=NULL) && curr->data == curr->next->data){
+           Node *temp= curr->next;
+            curr->next=temp->next;
+            delete temp;
+          
+        }
+        else{
+             curr=curr->next;
+        }
+       
+    }
+    return head;
+}
